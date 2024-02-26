754aef55dcb7074434a8a76bb8a1a1ec6d60f774
Shubham-391 <subhamy1234@gmail.com>
2024-02-26 11:48:49 +0530

    section3

--- assets/js/doc.js
+++ assets/js/doc.js
@@ -16,4 +16,50 @@ function navbar2() {
   navPage.classList.toggle("start-0");
   document.querySelector("body").classList.remove("overflow-hidden");
 }
+// slider1
+$(".slider1").slick({
+  infinite: true,
+  speed: 5000,
+  slidesToShow: 3.5,
+  slidesToScroll: 1,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 0,
+  cssEase: "Linear",
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 1150,
+      settings: {
+        slidesToShow: 2.5,
+      },
+    },
+    {
+      breakpoint: 850,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 1.5,
+      },
+    },
+    {
+      breakpoint: 450,
+      settings: {
+        slidesToShow: 1,
+        dots: true,
+        autoplay: false,
+        speed: 1000,
+      },
+    },
+  ],
+});
 
